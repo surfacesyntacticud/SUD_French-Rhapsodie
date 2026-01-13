86d5873f063e7987cf33d174be3a91d7f05221e1
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2026-01-13 15:13:13 +0100

    utils

--- utils/backport.py
+++ utils/backport.py
@@ -1,74 +1,45 @@
-# This script was used to backport morphological features update
-# from fr_rhapsodie.sud.*.conllu to prosody based
+from pathlib import Path
+from termcolor import colored
+import grewpy
+from grewpy import Corpus, CorpusDraft
+grewpy.set_config("sud")
 
-# the script was used to produce files for commit bedbad5eb7c98c729765d5d3ff33c33f6fafc411
+p_words_feats = ["AlignBegin", "AlignEnd", "AttachTo", "Filler", "Gender[ctxt]", "Gender[lex]", "HasSpokenGender", "HasSpokenNumber", "Idiom", "InIdiom", "LiaisonAfter", "LiaisonPossibleBefore", "Number[ctxt]", "Number[lex]", "Overlap", "PastPartHasSpokenGender", "Polite", "Rel", "SpaceAfter", "Subject", "Tense[denom]"]
 
-target_upos = ["NOUN", "VERB", "PRON", "PROPN", "ADJ"]
-
-# a tmp files build from the concat of fr_rhapsodie.sud.*.conllu (+ fake sent_id line at the end)
-with open("full_216.conllu", "r") as f:
-	ref_lines = [l.strip() for l in f.readlines()]
-
-def search(sent_id):
-	output = []
-	offset = ref_lines.index (f"# sent_id = {sent_id}")
-	line = ""
-	while not (line.startswith ("# sent_id = ")):
-		offset += 1
-		line = ref_lines[offset]
-		cols = line.split("\t")
-		if len(cols) == 10 and cols[3] in target_upos:
-			output.append (cols)
-	return output
-
-def split_fv(fv):
-	out = fv.split("=", 1)
-	if len(out) == 2:
-		return fv.split("=", 1)
-	else:
-		raise ValueError (fv)
-
-def split_misc(misc):
-	if misc == "_": return []
-	pairs = [split_fv(x) for x in misc.split("|")]
-	return {k: v for [k,v] in pairs}
-
-def update(file):
-	with open(file, "r") as f:
-		lines = [l.strip() for l in f.readlines()]
-
-	current_id = None
-	words = []
-	for position, line in enumerate(lines):
-		if line.startswith ("# old_id = "):
-			if len(words) != 0:
-				raise (ValueError (f"{current_id} ==> {words}"))
-			current_id = line[11:]
-			words = search(current_id)
-		cols = line.split("\t")
-		if len(cols) == 10 and cols[3] in target_upos:
-			if words[0][1] == cols[1] or words[0][1] == "-"+cols[1] or cols[1] == "auquel":
-				cols[1] = words[0][1]
-				cols[5] = words[0][5] # update FEATS column
-				old_misc = split_misc(cols[9])
-				new_misc = split_misc(words[0][9])
-				for k in new_misc:
-					if k not in old_misc:
-						old_misc[k] = new_misc[k]
-				cols[9] = "|".join([f"{k}={v}" for [k,v] in old_misc.items()])
-				lines[position] = "\t".join(cols)
-				del words[0]
-			else:
-				raise ValueError (f"{current_id} : {words[0][1]} ≠ {cols[1]}")
-	with open(file, "w") as f:
-		f.write ("\n".join(lines))
-		f.write ("\n")
+def backport (p_word, pauses, out):
+	data_p_word = Corpus(p_word)
+	data_pauses = CorpusDraft(pauses)
+	with open(out, "w") as f:
+		for s1, s2 in zip(data_p_word, data_pauses):
+			if s1 != s2:
+				raise ValueError (f"different sent_id: {s1} VS {s2}")
+			sent1 = data_p_word[s1]
+			sent2 = data_pauses[s2]
+			index1 = 0
+			for (id2,feat2) in sent2.features.items():
+				if '.' in id2: continue # skip syllable tokens
+				if feat2["form"] == "#": continue # skip pause tokens
+				if feat2["form"] != sent1[str(index1)]["form"]:  # Chekck that we are well aligned
+					raise ValueError (f'different words: {feat2["form"]} and {sent1[str(index1)]["form"]} in sent_id = {s1}')
+				all_keys = list(sent2[id2].keys())
+				keys_to_remove = [key for key in all_keys if key not in p_words_feats]
+				for key in keys_to_remove:
+					sent2[id2].pop(key)
 
 
-import glob
-
+				sent2[id2].update(sent1[str(index1)])
+				index1 += 1
+			f.write (sent2.to_conll())
+			f.write ("\n")
 if __name__ == "__main__":
-	for filepath in glob.iglob('Rhap*.conllu'):
-		update(filepath)
+	print(colored('WARNING: backport of relation of not implemented, only features!', 'yellow', 'on_red'))
 
-	# update("Rhap_M0024.conllu")
+	# backport ("p_word.conllu", "pauses.conllu", "out.conllu")
+	# backport ("../p_words/Rhap_D0001.conllu", "../prosody_pauses/Rhap_D0001.conllu", "../prosody_pauses/Rhap_D0001.conllu")
+
+	pathlist = Path("../p_words").glob('*.conllu')
+	for path in pathlist:
+		p_word_file = str(path)
+		pause_file = p_word_file.replace("p_words", "prosody_pauses")
+		print (p_word_file)
+		backport (p_word_file, pause_file, pause_file)
